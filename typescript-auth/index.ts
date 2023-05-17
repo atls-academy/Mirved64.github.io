7a95a2431c48b3dcb0a1508a9b9626d470dd0b65
Mirved64 <medmixal@yahoo.com>
2023-05-17 15:16:43 +0400

    feat: make verification of username length and password length

--- typescript-auth/index.ts
+++ typescript-auth/index.ts
@@ -26,16 +26,38 @@ function logout(): void {
 }
 
 function register(username: string, password: string): void {
-  const newUser: IUserInfo = {
-    username,
-    password,
-  };
+  try {
+    if (username.length <= 5) {
+      throw new Error("Username must be at least 5 characters long");
+    } else if (password.length <= 6) {
+      throw new Error("Password must be at least 6 characters long");
+    } else {
+      const newUser: IUserInfo = {
+        username,
+        password,
+      };
 
-  userIsAuth = { isAuth: true, user: newUser };
+      userIsAuth = { isAuth: true, user: newUser };
 
-  console.log(`Nice to meet you, ${username}`)
+      console.log(`Nice to meet you, ${username}`);
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message);
+    }
+  }
 }
 
 function whoami(): void {
-  console.log(`You are ${userIsAuth.user.username}`)
+  console.log(`You are ${userIsAuth.user.username}`);
 }
+
+whoami();
+logout();
+register("Mikhail", "qwe");
+register("Mixa", "qwerty");
+register("Mikhail", "qwerty1");
+whoami();
+logout();
+login("Mikhail", "qwerty");
+whoami();
