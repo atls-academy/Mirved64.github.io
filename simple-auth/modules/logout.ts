7246f4dbe4826cad96dca379f81ad0e4fb5c0f53
Mirved64 <medmixal@yahoo.com>
2023-05-23 13:47:24 +0400

    fix: fix logout module, yarn lint and yarn typecheck verification

--- simple-auth/modules/logout.ts
+++ simple-auth/modules/logout.ts
@@ -3,21 +3,22 @@ import { User } from "../types";
 
 export default function logout(usersList: User[]): User[] {
   try {
-
     const checkAuthUser: boolean = usersList.some(
       (user) => user.isAuth === true
     );
     if (!checkAuthUser) {
       throw new Error(`No authorized users`);
     }
-    
-    const logoutUser: number = usersList.findIndex(user => user.isAuth === true)
+
+    const logoutUser: number = usersList.findIndex(
+      (user) => user.isAuth === true
+    );
+    // eslint-disable-next-line no-param-reassign
     usersList[logoutUser].isAuth = false;
     // eslint-disable-next-line no-console
     console.log(`See you later, ${usersList[logoutUser].username}!`);
-    
   } catch (error) {
     handleError(error);
   }
-  return usersList
+  return usersList;
 }
