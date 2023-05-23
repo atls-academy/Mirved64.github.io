feb81e5b8e6738fe430516445f878c2a38379977
Mirved64 <medmixal@yahoo.com>
2023-05-23 11:54:25 +0400

    fix: fix whoami module

---
+++ simple-auth/whoami.ts
@@ -0,0 +1,19 @@
+import { LOGIN_BEFORE } from "./constants";
+import { handleError } from "./handle-error";
+import { User } from "./types";
+
+/*  eslint-disable @typescript-eslint/no-unused-vars  */
+export default function whoami(currentUser:User): void {
+  try {
+    if (!currentUser.isAuth) {
+      throw new Error(LOGIN_BEFORE);
+    }
+    // eslint-disable-next-line no-console
+    console.log(`You are ${currentUser.username}`);
+    // eslint-disable-next-line no-alert
+    alert(`You are ${currentUser.username}`);
+  } catch (error) {
+    handleError(error);
+  }
+}
+/*  eslint-enable @typescript-eslint/no-unused-vars */
