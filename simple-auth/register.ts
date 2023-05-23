c477053f1aa19145318835c19a4a67859d853eb6
Mirved64 <medmixal@yahoo.com>
2023-05-23 12:26:15 +0400

    fix: fix register module

---
+++ simple-auth/register.ts
@@ -0,0 +1,44 @@
+import { User } from "./types";
+import { handleError } from "./handle-error";
+
+export default function register(
+  username: string,
+  password: string,
+  usersList: User[]
+): User[] {
+  try{
+    const checkAuthUser: boolean = usersList.some((user) => user.isAuth === true);
+    if (checkAuthUser) {
+      throw new Error(`${username},  please logout before register!`);
+    }
+
+    if (username.length <= 5) {
+      throw new Error("Username must be at least 5 characters long");
+    }
+
+    const checkSameUsers: boolean = usersList.some(
+      (user) => user.username === username
+    );
+    if (checkSameUsers) {
+      throw new Error("A user with this name already exists");
+    }
+
+    if (password.length <= 6) {
+      throw new Error("Password must be at least 6 characters long");
+    }
+
+    const newReg: User = {
+      username,
+      password,
+      isAuth: true,
+    };
+
+    usersList.push(newReg);
+    // eslint-disable-next-line no-alert
+    alert(`Welcome, ${newReg.username}!`);
+    
+  } catch (error) {
+    handleError(error);
+  }
+  return usersList;
+}
