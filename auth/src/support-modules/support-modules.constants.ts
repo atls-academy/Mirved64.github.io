f95038fd6908fd75a9a3794b861946f039d02c88
Mirved64 <medmixal@yahoo.com>
2023-05-31 13:39:47 +0400

    fix: remove constants file

--- auth/src/support-modules/support-modules.constants.ts
+++
@@ -1,3 +0,0 @@
-export const WORK_STOP: string = "Work was stopped by the user";
-export const ENTER_USERNAME: string = "Enter your username, please";
-export const ENTER_PASSWORD: string = "Enter password, please";
