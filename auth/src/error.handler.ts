262076572d6a035a98e010b11561124592c31e55
Mirved64 <medmixal@yahoo.com>
2023-06-06 15:29:47 +0400

    fix: rename handelError to error.handler

---
+++ auth/src/error.handler.ts
@@ -0,0 +1,4 @@
+export const errorHandler = (name: string, message: string): void => {
+  // eslint-disable-next-line no-console
+  console.error(`${name} ${message}`)
+}
--- auth/src/handle-error.ts
+++
@@ -1,6 +0,0 @@
-export const handleError = (error: unknown): void => {
-  if (error instanceof Error) {
-    // eslint-disable-next-line no-console
-    console.error(error.message)
-  }
-}
