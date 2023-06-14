894059f1dda58ad44f829725afa637c63af7acae
Mirved64 <medmixal@yahoo.com>
2023-06-14 11:56:25 +0400

    fix: fix font-sizes and line-heights

--- landing/ui/theme/src/theme/font-sizes.ts
+++ landing/ui/theme/src/theme/font-sizes.ts
@@ -1,15 +1,15 @@
-const fontSize = {
-  atom: 14,
-  small: 16,
-  normal: 18,
-  big: 20,
-  large: 24,
-  xl: 32,
-  extraLarge: 40,
-  giant: 44,
-  super: 64,
-  extraSuper: 80,
-  biggest: 96,
+const fontSizes = {
+  atom: '14px',
+  small: '16px',
+  normal: '18px',
+  big: '20px',
+  large: '24px',
+  xl: '32px',
+  extraLarge: '40px',
+  giant: '44px',
+  super: '64px',
+  extraSuper: '80px',
+  biggest: '96px',
 }
 
-export { fontSize }
+export { fontSizes }
