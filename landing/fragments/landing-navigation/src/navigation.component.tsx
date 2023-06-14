ad099c82964ae5c5ef01ea49c6da9c410f018369
Mirved64 <medmixal@yahoo.com>
2023-06-14 15:27:54 +0400

    feat: try plugin ui/link, ui/icons to navigation section

--- landing/fragments/landing-navigation/src/navigation.component.tsx
+++ landing/fragments/landing-navigation/src/navigation.component.tsx
@@ -13,13 +13,13 @@ export const Navigation = () => (
     margin='0 auto'
   >
     <Box border='solid 2px red' width='56px' height='56px' marginLeft='40px'>
-      Text
+      TextText
     </Box>
     <Box border='solid 2px green' width='860px' height='18px'>
-      Text
+      TextText
     </Box>
     <Box border='solid 2px blue' width='54px' height='18px' marginRight='40px'>
-      Text
+      TextText
     </Box>
   </Box>
 )
