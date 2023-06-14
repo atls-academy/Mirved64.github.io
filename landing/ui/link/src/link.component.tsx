a2df599c8717375368c5b7d8f1d06b8ba596fe42
Mirved64 <medmixal@yahoo.com>
2023-06-14 18:45:42 +0400

    fix: fix ui/link. make link.component

---
+++ landing/ui/link/src/link.component.tsx
@@ -0,0 +1,18 @@
+import styled               from '@emotion/styled'
+import { createBaseStyles } from '@atls-ui-parts/link'
+import { createNextLink }   from '@atls-ui-parts/link'
+
+import React                from 'react'
+
+import { Text }             from '@ui/text'
+
+const BaseLink = styled(Text.withComponent('a'))<{ active: boolean; theme: any }>(
+  createBaseStyles()
+)
+
+export const Link = (props) => <BaseLink {...props} />
+
+export const NextLink = (props) => {
+  const NextLinkRenderer = createNextLink(BaseLink)
+  return <NextLinkRenderer {...props} />
+}
