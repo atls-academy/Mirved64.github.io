50227df882350f5adf4050457fdece7715fa16cb
Mirved64 <medmixal@yahoo.com>
2023-06-26 16:01:06 +0400

    fix: delete comments

--- landing/entrypoints/renderer/next.config.js
+++ landing/entrypoints/renderer/next.config.js
@@ -8,13 +8,4 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // TODO check webpack config settings
-  // webpack: (config) => {
-  //   // eslint-disable-next-line
-  //   config.resolve.alias['@emotion/react'] = require.resolve('@emotion/react')
-  //   // eslint-disable-next-line
-  //   config.resolve.alias['@emotion/styled'] = require.resolve('@emotion/styled')
-
-  //   return config
-  // },
 }
