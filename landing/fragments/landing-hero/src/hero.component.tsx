0adb7f99a14aa4c8e8a42b873343e804c2b39e10
Mirved64 <medmixal@yahoo.com>
2023-07-26 15:42:37 +0400

    feat: wrap socials to nextlink in hero section, check markup

--- landing/fragments/landing-hero/src/hero.component.tsx
+++ landing/fragments/landing-hero/src/hero.component.tsx
@@ -10,6 +10,7 @@ import { Box }              from '@ui/layout'
 import { Row }              from '@ui/layout'
 import { Layout }           from '@ui/layout'
 import { Column }           from '@ui/layout'
+import { NextLink }         from '@ui/link'
 import { Text }             from '@ui/text'
 import { Space }            from '@ui/text'
 
@@ -17,11 +18,11 @@ export const Hero = () => (
   <Box backgroundColor='background.hero' width='100%' flexDirection='column' margin='0 auto'>
     <Layout flexBasis={[80, 412]} />
 
-    <Row flexGrow='1' flexShrink='1'>
-      <Layout flexBasis={[20, 230]} flexShrink='0' />
+    <Row flexGrow='1'>
+      <Layout flexBasis={[20, 230]} />
 
-      <Row flexGrow='1' flexShrink='1'>
-        <Column flexShrink='1' flexGrow='1' flexBasis={[335, 1460]}>
+      <Row >
+        <Column flexBasis={[335, 1460]} flexGrow={[1, 0]}>
           <Box>
             <Text
               color='text.hero.primary'
@@ -70,13 +71,13 @@ export const Hero = () => (
             </Box>
           </Row>
 
-          <Layout flexBasis={[24, 48]} flexShrink='1' />
+          <Layout flexBasis={[24, 48]} />
 
-          <Divider backgroundColor='divider.primary' weight={1} flexShrink='1' />
+          <Divider backgroundColor='divider.primary' weight={1} />
 
-          <Layout flexBasis={[24, 48]} flexShrink='1' />
+          <Layout flexBasis={[24, 48]} />
 
-          <Box flexWrap='wrap' maxWidth={640} flexShrink='1'>
+          <Box flexWrap='wrap' maxWidth={[335, 640]}>
             <Text
               color='text.hero.primary'
               fontSize={['tiny', 'small']}
@@ -91,82 +92,93 @@ export const Hero = () => (
 
           <Row
             display={['flex', 'none']}
-            flexShrink='1'
             flexGrow='1'
             flexBasis={44}
             justifyContent='space-between'
             alignItems='center'
           >
             <Box>
-              <Button
-                variant='transparentBackgroundWhiteText'
-                fill
-                icon={<TelegramIcon width={15} height={12} />}
-                widthIcon={104}
-                heightIcon={44}
-              />
+              <NextLink path='https://web.telegram.org/'>
+                <Button
+                  variant='transparentBackgroundWhiteText'
+                  fill
+                  icon={<TelegramIcon width={15} height={12} />}
+                  widthIcon={104}
+                  heightIcon={44}
+                />
+              </NextLink>
             </Box>
 
             <Box>
-              <Button
-                variant='transparentBackgroundWhiteText'
-                fill
-                icon={<GitHubIcon width={15} height={15} />}
-                widthIcon={104}
-                heightIcon={44}
-              />
+              <NextLink path='https://github.com/'>
+                <Button
+                  variant='transparentBackgroundWhiteText'
+                  fill
+                  icon={<GitHubIcon width={15} height={15} />}
+                  widthIcon={104}
+                  heightIcon={44}
+                />
+              </NextLink>
             </Box>
 
             <Box>
-              <Button
-                variant='transparentBackgroundWhiteText'
-                fill
-                icon={<MailWhiteIcon width={18} height={14} />}
-                widthIcon={104}
-                heightIcon={44}
-              />
+              <NextLink path='https://www.google.com/intl/ru/gmail/about/'>
+                <Button
+                  variant='transparentBackgroundWhiteText'
+                  fill
+                  icon={<MailWhiteIcon width={18} height={14} />}
+                  widthIcon={104}
+                  heightIcon={44}
+                />
+              </NextLink>
             </Box>
           </Row>
         </Column>
 
-        <Layout flexBasis={[0, 134]} />
+        <Layout flexBasis={[0, 134]} flexGrow={[0, 1]} />
 
         <Box display={['none', 'flex']} alignItems='end' flexBasis={56}>
           <Column height='200px' justifyContent='space-around' alignItems='center'>
             <Box>
-              <Button
-                variant='transparentBackgroundWhiteText'
-                fill
-                icon={<TelegramIcon width={18} height={15} />}
-                widthIcon={56}
-                heightIcon={56}
-              />
+              <NextLink path='https://web.telegram.org/'>
+                <Button
+                  variant='transparentBackgroundWhiteText'
+                  fill
+                  icon={<TelegramIcon width={18} height={15} />}
+                  widthIcon={56}
+                  heightIcon={56}
+                />
+              </NextLink>
             </Box>
 
             <Box>
-              <Button
-                variant='transparentBackgroundWhiteText'
-                fill
-                icon={<GitHubIcon width={18} height={18} />}
-                widthIcon={56}
-                heightIcon={56}
-              />
+              <NextLink path='https://github.com/'>
+                <Button
+                  variant='transparentBackgroundWhiteText'
+                  fill
+                  icon={<GitHubIcon width={18} height={18} />}
+                  widthIcon={56}
+                  heightIcon={56}
+                />
+              </NextLink>
             </Box>
 
             <Box>
-              <Button
-                variant='transparentBackgroundWhiteText'
-                fill
-                icon={<MailWhiteIcon width={18} height={14} />}
-                widthIcon={56}
-                heightIcon={56}
-              />
+              <NextLink path='https://www.google.com/intl/ru/gmail/about/'>
+                <Button
+                  variant='transparentBackgroundWhiteText'
+                  fill
+                  icon={<MailWhiteIcon width={18} height={14} />}
+                  widthIcon={56}
+                  heightIcon={56}
+                />
+              </NextLink>
             </Box>
           </Column>
         </Box>
       </Row>
 
-      <Layout flexBasis={[20, 40]} flexShrink='0' />
+      <Layout flexBasis={[20, 40]} />
     </Row>
 
     <Layout flexBasis={[20, 50]} />
