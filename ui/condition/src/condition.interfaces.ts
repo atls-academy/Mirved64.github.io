0d3b70284a7ffd16d2d4e52090bbdb022bf610ab
Mirved64 <medmixal@yahoo.com>
2023-08-29 11:46:31 +0400

    refactor: rework ui/condition

--- ui/condition/src/condition.interfaces.ts
+++ ui/condition/src/condition.interfaces.ts
@@ -1,11 +1,4 @@
-export interface SmoothOptions {
-  duration?: number
-  pattern?: 'in' | 'out' | 'in-and-out'
-}
-
 export interface ConditionProps {
   match: boolean
-  smooth?: boolean
-  smoothOptions?: SmoothOptions
   children?: any
 }
