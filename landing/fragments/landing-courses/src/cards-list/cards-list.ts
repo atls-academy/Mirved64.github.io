f0b54d0820bec83875ebbf01222bf6cfabf46958
Mirved64 <medmixal@yahoo.com>
2023-09-01 20:41:23 +0400

    refactor: use enum in cards interface

--- landing/fragments/landing-courses/src/cards-list/cards-list.ts
+++ landing/fragments/landing-courses/src/cards-list/cards-list.ts
@@ -1,7 +1,13 @@
+enum CardCategory { // eslint-disable-line
+  Teach = 'обучение',
+  MiniCourse = 'мини-курс',
+  EducationalMaterial = 'учебный материал',
+}
+
 interface CardProps {
   id: number
   isMobileOnly?: boolean
-  category: string
+  category: CardCategory
   title: string
   descriptionMobile?: string
   descriptionDesktop?: string
@@ -11,7 +17,7 @@ export const CadrsList: CardProps[] = [
   {
     id: 0,
     isMobileOnly: false,
-    category: 'обучение',
+    category: CardCategory.Teach,
     title: 'Frontend Engineer',
     descriptionMobile:
       'Вы изучите фундаментальные знания в ИТ и прокачаетесь в направлении программирования. Получите востребованную профессию и возможность открыть бизнес в сфере информационных технологий',
@@ -21,7 +27,7 @@ export const CadrsList: CardProps[] = [
   {
     id: 1,
     isMobileOnly: false,
-    category: 'мини-курс',
+    category: CardCategory.MiniCourse,
     title: 'Основы разработки',
     descriptionMobile:
       'Вы изучите фундаментальные знания в ИТ и прокачаетесь в направлении программирования. Получите востребованную профессию и возможность открыть бизнес в сфере информационных технологий',
@@ -31,25 +37,25 @@ export const CadrsList: CardProps[] = [
   {
     id: 2,
     isMobileOnly: false,
-    category: 'учебный материал',
+    category: CardCategory.EducationalMaterial,
     title: 'Как использовать библиотеки',
   },
   {
     id: 3,
     isMobileOnly: false,
-    category: 'учебный материал',
+    category: CardCategory.EducationalMaterial,
     title: 'Дизайн, Figma, Математика',
   },
   {
     id: 4,
     isMobileOnly: true,
-    category: 'учебный материал',
+    category: CardCategory.EducationalMaterial,
     title: 'Как использовать библиотеки',
   },
   {
     id: 5,
     isMobileOnly: true,
-    category: 'учебный материал',
+    category: CardCategory.EducationalMaterial,
     title: 'Дизайн, Figma, Математика',
   },
 ]
