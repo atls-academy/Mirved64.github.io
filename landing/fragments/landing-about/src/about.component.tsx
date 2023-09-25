51afbf85efc4f15dfde9ff8df5231030915177c7
Mirved64 <medmixal@yahoo.com>
2023-09-25 11:31:19 +0400

    refactor: add text components in about section

--- landing/fragments/landing-about/src/about.component.tsx
+++ landing/fragments/landing-about/src/about.component.tsx
@@ -1,4 +1,5 @@
 import React                from 'react'
+import { FC }               from 'react'
 import { FormattedMessage } from 'react-intl'
 
 import { Delimiter }        from '@ui/delimiter'
@@ -9,116 +10,88 @@ import { Column }           from '@ui/layout'
 import { Text }             from '@ui/text'
 import { Space }            from '@ui/text'
 
-export const About = () => (
-  <Column flexGrow='1'>
-    <Layout flexBasis={[64, 160]} />
+import { TextProps }        from './about.interfaces'
 
-    <Row>
-      <Layout flexBasis={[20, 230]} />
+export const About = () => {
+  const DesktopText: FC<TextProps> = ({ id }) => (
+    <Text
+      display='inline'
+      color='text.white'
+      fontSize='common'
+      lineHeight='huge'
+      wordWrap='break-word'
+    >
+      <FormattedMessage id={id} />
+    </Text>
+  )
 
-      <Column flexBasis={[335, 1460]} flexGrow='1' flexShrink='0'>
-        <Box display={['none', 'inline']}>
-          <Text
-            display='inline'
-            color='text.white'
-            fontSize='common'
-            lineHeight='huge'
-            wordWrap='break-word'
-          >
-            <FormattedMessage id='about.intro-desktop' />
-          </Text>
+  const MobileText: FC<TextProps> = ({ id }) => (
+    <Box>
+      <Text color='text.white' fontSize='small' lineHeight='huge'>
+        <FormattedMessage id={id} />
+      </Text>
+    </Box>
+  )
 
-          <Space count='6' />
+  return (
+    <Column flexGrow='1'>
+      <Layout flexBasis={[64, 160]} />
 
-          <Delimiter />
+      <Row>
+        <Layout flexBasis={[20, 230]} />
 
-          <Space count='6' />
+        <Column flexBasis={[335, 1460]} flexGrow='1' flexShrink='0'>
+          <Box display={['none', 'inline']}>
+            <DesktopText id='about.intro-desktop' />
 
-          <Text
-            display='inline'
-            color='text.white'
-            fontSize='common'
-            lineHeight='huge'
-            wordWrap='break-word'
-          >
-            <FormattedMessage id='about.problem-desktop' />
-          </Text>
+            <Space count='6' />
 
-          <Space count='6' />
+            <Delimiter />
 
-          <Delimiter />
-        </Box>
+            <Space count='6' />
 
-        <Column display={['flex', 'none']}>
-          <Box>
-            <Text color='text.white' fontSize='small' lineHeight='huge'>
-              <FormattedMessage id='about.intro-digital-mobile' />
-            </Text>
-          </Box>
-
-          <Box>
-            <Text color='text.white' fontSize='small' lineHeight='huge'>
-              <FormattedMessage id='about.intro-not-decrease-mobile' />
-            </Text>
-          </Box>
-
-          <Box>
-            <Text color='text.white' fontSize='small' lineHeight='huge'>
-              <FormattedMessage id='about.intro-for-your-decisions-mobile' />
-            </Text>
-          </Box>
-
-          <Layout flexBasis={28} />
-
-          <Box>
-            <Text color='text.white' fontSize='small' lineHeight='huge'>
-              <FormattedMessage id='about.central-idea-atlantis-teach-mobile' />
-            </Text>
-          </Box>
-
-          <Box>
-            <Text color='text.white' fontSize='small' lineHeight='huge'>
-              <FormattedMessage id='about.central-idea-make-engineers-mobile' />
-            </Text>
-          </Box>
-
-          <Box>
-            <Text color='text.white' fontSize='small' lineHeight='huge'>
-              <FormattedMessage id='about.central-idea-architects-mobile' />
-            </Text>
-          </Box>
-
-          <Box>
-            <Text color='text.white' fontSize='small' lineHeight='huge'>
-              <FormattedMessage id='about.central-idea-not-programmers-mobile' />
-            </Text>
-          </Box>
-        </Column>
-
-        <Layout flexBasis={[0, 40]} />
-
-        <Box display={['none', 'inline']}>
-          <Box display='inline'>
-            <Text
-              display='inline'
-              color='text.white'
-              fontSize='common'
-              lineHeight='huge'
-              wordWrap='break-word'
-            >
-              <FormattedMessage id='about.central-idea-desktop' />
-            </Text>
+            <DesktopText id='about.problem-desktop' />
 
             <Space count='6' />
 
             <Delimiter />
           </Box>
-        </Box>
-      </Column>
 
-      <Layout flexBasis={[20, 230]} />
-    </Row>
+          <Column display={['flex', 'none']}>
+            <MobileText id='about.intro-digital-mobile' />
 
-    <Layout flexBasis={[128, 240]} />
-  </Column>
-)
+            <MobileText id='about.intro-not-decrease-mobile' />
+
+            <MobileText id='about.intro-for-your-decisions-mobile' />
+
+            <Layout flexBasis={28} />
+
+            <MobileText id='about.central-idea-atlantis-teach-mobile' />
+
+            <MobileText id='about.central-idea-make-engineers-mobile' />
+
+            <MobileText id='about.central-idea-architects-mobile' />
+
+            <MobileText id='about.central-idea-not-programmers-mobile' />
+          </Column>
+
+          <Layout flexBasis={[0, 40]} />
+
+          <Box display={['none', 'inline']}>
+            <Box display='inline'>
+              <DesktopText id='about.central-idea-desktop' />
+
+              <Space count='6' />
+
+              <Delimiter />
+            </Box>
+          </Box>
+        </Column>
+
+        <Layout flexBasis={[20, 230]} />
+      </Row>
+
+      <Layout flexBasis={[128, 240]} />
+    </Column>
+  )
+}
