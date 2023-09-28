43cf6ef68ffd9af655310150441af27f00d3dca0
Mirved64 <medmixal@yahoo.com>
2023-09-28 09:09:39 +0400

    chore: rework social link component

--- landing/fragments/landing-hero/src/social-link/social-link.component.tsx
+++ landing/fragments/landing-hero/src/social-link/social-link.component.tsx
@@ -10,50 +10,40 @@ import { SocialLinkDisplay } from './social-link.interface'
 import { SocialLinkProps }   from './social-link.interface'
 
 export const SocialLink: FC<SocialLinkProps> = ({ display, icon, href }) => (
-  <>
-    <Condition match={display === SocialLinkDisplay.Desktop}>
-      <Box>
-        <Link href={href}>
-          <Button
-            variant='ghostBackgroundWhiteText'
-            size='bigSizeNormalRadii'
-            icon={icon}
-            widthIcon={56}
-            heightIcon={56}
-            radiiIcon='normal'
-          />
-        </Link>
-      </Box>
-    </Condition>
+  <Box>
+    <Link href={href} target='_blank'>
+      <Condition match={display === SocialLinkDisplay.Desktop}>
+        <Button
+          variant='ghostBackgroundWhiteText'
+          size='bigSizeNormalRadii'
+          icon={icon}
+          widthIcon={56}
+          heightIcon={56}
+          radiiIcon='normal'
+        />
+      </Condition>
 
-    <Condition match={display === SocialLinkDisplay.Mobile}>
-      <Box>
-        <Link href={href}>
-          <Button
-            variant='ghostBackgroundWhiteText'
-            size='littleSizeLittleRadii'
-            icon={icon}
-            widthIcon={104}
-            heightIcon={44}
-            radiiIcon='little'
-          />
-        </Link>
-      </Box>
-    </Condition>
+      <Condition match={display === SocialLinkDisplay.Mobile}>
+        <Button
+          variant='ghostBackgroundWhiteText'
+          size='littleSizeLittleRadii'
+          icon={icon}
+          widthIcon={104}
+          heightIcon={44}
+          radiiIcon='little'
+        />
+      </Condition>
 
-    <Condition match={display === SocialLinkDisplay.Ultra}>
-      <Box>
-        <Link href={href}>
-          <Button
-            variant='ghostBackgroundWhiteText'
-            size='strongSizeRegularRadii'
-            icon={icon}
-            widthIcon={84}
-            heightIcon={84}
-            radiiIcon='normal'
-          />
-        </Link>
-      </Box>
-    </Condition>
-  </>
+      <Condition match={display === SocialLinkDisplay.Ultra}>
+        <Button
+          variant='ghostBackgroundWhiteText'
+          size='strongSizeRegularRadii'
+          icon={icon}
+          widthIcon={84}
+          heightIcon={84}
+          radiiIcon='normal'
+        />
+      </Condition>
+    </Link>
+  </Box>
 )
