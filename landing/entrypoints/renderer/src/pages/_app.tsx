f00027e1f2dcd46e692d00bd85ef8995d2f503db
Mirved64 <medmixal@yahoo.com>
2023-10-02 18:26:26 +0400

    feat: add @apollo/client in landing/entrypoints

--- landing/entrypoints/renderer/src/pages/_app.tsx
+++ landing/entrypoints/renderer/src/pages/_app.tsx
@@ -1,19 +1,28 @@
 import * as messages        from '../../locales/ru.json'
 
+import { ApolloProvider }   from '@apollo/client'
+
 import React                from 'react'
 import { IntlProvider }     from 'react-intl'
 import { ParallaxProvider } from 'react-scroll-parallax'
 
 import { ThemeProvider }    from '@ui/theme'
+import { getClient }        from '@globals/data'
 
-const App = ({ Component, pageProps, ...props }) => (
-  <IntlProvider messages={messages} locale='ru' defaultLocale='ru'>
-    <ThemeProvider>
-      <ParallaxProvider>
-        <Component {...pageProps} {...props} />
-      </ParallaxProvider>
-    </ThemeProvider>
-  </IntlProvider>
-)
+const App = ({ Component, pageProps, ...props }) => {
+  const client = getClient()
+
+  return (
+    <ApolloProvider client={client}>
+      <IntlProvider messages={messages} locale='ru' defaultLocale='ru'>
+        <ThemeProvider>
+          <ParallaxProvider>
+            <Component {...pageProps} {...props} />
+          </ParallaxProvider>
+        </ThemeProvider>
+      </IntlProvider>
+    </ApolloProvider>
+  )
+}
 
 export default App
